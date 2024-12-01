e29e15f825d68e5fb185c0f2860051b5df0a38d3
Andre Luis da Cunha <andreluiscunha81@gmail.com>
2024-12-01 19:59:55 -0300

    feat: implement basic TCP server
    - close #2

--- cmd/ottermq/main.go
+++ cmd/ottermq/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/andrelcunha/ottermq/internal/broker"
+	"github.com/andrelcunha/ottermq/pkg/config"
+)
 
 func main() {
 	fmt.Println("Starting OtterMq...")
+
+	// Handle OS signals for graceful shutdown
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
+	// Initialize configuration
+	config := &config.Config{
+		Port: 5672,
+	}
+	fmt.Println("Config Port: ", config.Port)
+
+	// Initialize the message broker
+	broker := broker.NewBroker(config)
+	go broker.Start()
+
+	// Wait for a signal to shutdown
+	<-signals
+	fmt.Println("Shutting down OtterMq...")
+	broker.Stop()
 }
